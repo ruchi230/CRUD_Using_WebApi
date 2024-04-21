5f8dcbd1ac2061e34ec5fc2b0a3ea18fcad3a13b
Divyesh Patel <Divyesh Patel@DIVYESH>
2024-04-21 16:57:27 +0530

    Add project files.

---
+++ MyAppApi/Controllers/DemoController.cs
@@ -0,0 +1,81 @@
+﻿using MyAppApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyAppApi.Controllers
+{
+    public class DemoController : ApiController
+    {
+        private employeeEntities1 db = new employeeEntities1();
+
+        //returns all user details
+
+        [HttpGet]
+        public List<UserDetail> GetAllUser()
+        {
+            return db.UserDetails.ToList();
+        }
+
+        //get userdetail by id
+        [HttpGet]
+        public UserDetail GetUserById(int id)
+        {
+            return db.UserDetails.Find(id);
+        }
+
+        [HttpPost]
+        public string AddUser(UserDetail user)
+        {
+            if(user != null && !string.IsNullOrEmpty(user.Name) && !string.IsNullOrEmpty(user.City) && !string.IsNullOrEmpty(user.Age))
+            {
+                db.UserDetails.Add(user);
+                db.SaveChanges();
+                return "User details added successfully";
+                /*int UserId = db.UserDetails.OrderByDescending(x => x.Id).First().Id;
+                return UserId;*/
+            }
+            else
+            {
+                return "Please enter proper details";
+            }
+        }
+        [HttpPut]
+        public string UpdateUser(int id,UserDetail user)
+        {
+            if(id > 0 && user != null && !string.IsNullOrEmpty(user.Name) && !string.IsNullOrEmpty(user.City) && !string.IsNullOrEmpty(user.Age))
+            {
+                UserDetail uDetail = db.UserDetails.Find(id);
+                uDetail.Name = user.Name;
+                uDetail.City = user.City;
+                uDetail.Age = user.Age;
+                db.SaveChanges();
+                return "User detail Updated!!";
+            }
+            else
+            {
+                return "Please enter proper details";
+            }
+        }
+        [HttpDelete]
+        public string DeleteUser(int id)
+        {
+            if(id > 0)
+            {
+                UserDetail uDetail = db.UserDetails.Find(id);
+                db.UserDetails.Remove(uDetail);
+                db.SaveChanges();
+                return "User details removed successfully";
+            }
+            else
+            {
+                return "please enter proper details";
+            }
+ 
+        }
+        
+    }
+}
---
+++ MyAppApi/Controllers/UserController.cs
@@ -0,0 +1,89 @@
+﻿using MyAppApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MyAppApi.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44305/api");
+        HttpClient client;
+
+        public UserController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            List<UserDetail> list = new List<UserDetail>();
+
+            var response = client.GetAsync(client.BaseAddress + "/demo/getalluser").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                list = JsonConvert.DeserializeObject<List<UserDetail>>(data);
+            }
+            return View(list);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(UserDetail model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data,Encoding.UTF8,"application/json");
+
+            var resopnse = client.PostAsync(client.BaseAddress + "/demo/AddUser",content).Result;
+            if(resopnse.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            UserDetail user = new UserDetail();
+
+            var response = client.GetAsync(client.BaseAddress + "/demo/GetUserById/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                user = JsonConvert.DeserializeObject<UserDetail>(data);
+            }
+            return View("Create",user);
+        }
+        [HttpPost]
+        public ActionResult Edit(UserDetail model)
+        {
+            string data = JsonConvert.SerializeObject(model);
+            StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            var response = client.PutAsync(client.BaseAddress + "/demo/UpdateUser/" +  model.Id,content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var response = client.DeleteAsync(client.BaseAddress + "/demo/DeleteUser/" +id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
